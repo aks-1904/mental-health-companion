9e6bad20d837e4827de0b5edaefb4d4c48e193da
Akshay Sharma <akshaysharma.work.1904@gmail.com>
2026-01-26 16:43:16 +0530

    login api creation:

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -1,9 +1,11 @@
-import { Request, Response } from "express";
+import { CookieOptions, Request, Response } from "express";
 import { printError } from "../utils/dev.js";
 import {
   RegisterResponse,
   RegisterRequestData,
   VerifyEmailResponse,
+  LoginResponse,
+  LoginRequestData,
 } from "../types/data.js";
 import User from "../models/user.model.js";
 import { validateEmail, validateName } from "../utils/validators.js";
@@ -12,12 +14,15 @@ import { compareHashedString, generateDate, hashText } from "../utils/util.js";
 import EmailVerification from "../models/emailVerfication.model.js";
 import { sendEmail } from "../services/email.service.js";
 import { verifyEmailTemplate } from "../emails/verify-mail.js";
+import jwt from "jsonwebtoken";
 
 const OTP_EXPIRING_TIME = 10; // In minutes
+const JWT_SECRET = process.env.JWT_SECRET;
+const isProduction = process.env.NODE_END === "production";
 
 export const register = async (
   req: Request,
-  res: Response
+  res: Response,
 ): Promise<Response<RegisterResponse>> => {
   try {
     const { email, password, name }: RegisterRequestData = req.body;
@@ -99,7 +104,7 @@ export const register = async (
 
 export const verifyEmail = async (
   req: Request,
-  res: Response
+  res: Response,
 ): Promise<Response<VerifyEmailResponse>> => {
   try {
     const { otp, userId } = req.body;
@@ -159,5 +164,67 @@ export const verifyEmail = async (
       success: false,
       message: error?.message || "Failed to verify you email",
     });
+  }
+};
+
+export const login = async (
+  req: Request<{}, {}, LoginRequestData>,
+  res: Response<LoginResponse>,
+): Promise<void> => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      // If user with given email is not found
+      res.status(404).json({
+        success: false,
+        message: "Email or password is invalid",
+      });
+      return;
+    }
+
+    const isValidPass = compareHashedString(password, user.passwordHash);
+    if (!isValidPass) {
+      // Given password doesn't match with database
+      res.status(401).json({
+        success: false,
+        message: "Email or password is invalid",
+      });
+      return;
+    }
+
+    if (!user.verified) {
+      // Email isn't verified
+      res.status(401).json({
+        success: false,
+        message: "Your account isn't verified, first verify your account",
+      });
+    }
+
+    const token = jwt.sign(user._id, JWT_SECRET as string); // Generate token value
+    const tokenOptions: CookieOptions = {
+      httpOnly: true,
+      secure: isProduction,
+      sameSite: isProduction ? "strict" : "lax",
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+      domain: isProduction ? process.env.DOMAIN : undefined,
+    };
+
+    // Seeting cookie to client
+    res.cookie("token", token, tokenOptions);
+
+    res.status(200).json({
+      success: false,
+      message: "Logged in successfully",
+    });
+    return;
+  } catch (error: any) {
+    printError(error);
+    res.status(500).json({
+      success: false,
+      message: error?.message || "Unable to login to your account",
+    });
+    return;
   }
 };
--- server/src/types/data.ts
+++ server/src/types/data.ts
@@ -13,10 +13,19 @@ export interface VerifyEmailResponse extends BaseAuthResponse {
   profile?: IUser;
 }
 
+export interface LoginResponse extends BaseAuthResponse {
+  profile?: IUser;
+}
+
 export interface RegisterRequestData {
   email: string;
   password: string;
   name: string;
+}
+
+export interface LoginRequestData {
+  email: string;
+  password: string;
 }
 
 export type SendEmailParams = {
